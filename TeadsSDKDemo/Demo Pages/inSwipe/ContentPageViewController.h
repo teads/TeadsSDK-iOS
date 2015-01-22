65e58cc7a53d36c4e738173d7b729f8769e6f0b1
Nikolaï Roycourt <nikolai.roycourt@ebuzzing.com>
2015-01-22 14:11:44 +0100

    inSwipe detail pages classes moved

---
+++ TeadsSDKDemo/Demo Pages/inSwipe/ContentPageViewController.h
@@ -0,0 +1,20 @@
+//
+//  ContentPageViewController.h
+//  TeadsSDKDemo
+//
+//  Created by Nikolaï Roycourt on 19/01/2015.
+//  Copyright (c) 2015 Teads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentPageViewController : UIViewController
+
+
+@property (assign, nonatomic) NSInteger index;
+
+@property (strong, nonatomic) IBOutlet UILabel *viewLabel;
+@property (nonatomic, strong) IBOutlet UIImageView *arrowLeft;
+@property (nonatomic, strong) IBOutlet UIImageView *arrowRight;
+
+@end
---
+++ TeadsSDKDemo/Demo Pages/inSwipe/ContentPageViewController.m
@@ -0,0 +1,44 @@
+//
+//  ContentPageViewController.m
+//  TeadsSDKDemo
+//
+//  Created by Nikolaï Roycourt on 19/01/2015.
+//  Copyright (c) 2015 Teads. All rights reserved.
+//
+
+#import "ContentPageViewController.h"
+
+@interface ContentPageViewController ()
+
+@end
+
+@implementation ContentPageViewController
+@synthesize viewLabel, arrowLeft, arrowRight;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.viewLabel.text = [NSString stringWithFormat:@"%ld", (long)self.index];
+    if (self.index >= 2) {
+        self.arrowRight.hidden = YES;
+        self.arrowLeft.hidden = NO;
+    } else {
+        self.arrowRight.hidden = NO;
+        self.arrowLeft.hidden = YES;
+    }
+}
+
+
+@end
