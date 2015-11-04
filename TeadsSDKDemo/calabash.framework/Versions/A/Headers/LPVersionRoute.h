410b39f98f4e99c6d2dc260a6b69af3009f33582
Ibrahim Ennafaa <ibrahim.ennafaa@ebuzzing.com>
2015-11-04 10:36:27 +0100

    update calabash framework

--- TeadsSDKDemo/calabash.framework/Versions/A/Headers/LPVersionRoute.h
+++ TeadsSDKDemo/calabash.framework/Versions/A/Headers/LPVersionRoute.h
@@ -21,7 +21,7 @@
  Do not change the 'CALABASH VERSION' portion of the following constant without
  updating the ruby API.
  ******************/
-#define kLPCALABASHVERSION @"CALABASH VERSION: 0.14.3"
+#define kLPCALABASHVERSION @"CALABASH VERSION: 0.16.4"
 
 @interface LPVersionRoute : NSObject <LPRoute>
 
