0590ca976d09a6e84b9a0c4caa130d80cb7da9ea
Grosjean Jérémy <jeremy.grosjean@teads.tv>
2018-04-13 16:49:23 +0200

    V4.0.0-beta

---
+++ TeadsSDK.framework/Headers/TeadsSDK.h
@@ -0,0 +1,18 @@
+//
+//  TeadsSDK.h
+//  TeadsSDK
+//
+//  Created by Bastien Lefrant on 31/01/2017.
+//  Copyright © 2017 Teads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TeadsSDK.
+FOUNDATION_EXPORT double TeadsSDKVersionNumber;
+
+//! Project version string for TeadsSDK.
+FOUNDATION_EXPORT const unsigned char TeadsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TeadsSDK/PublicHeader.h>
+
