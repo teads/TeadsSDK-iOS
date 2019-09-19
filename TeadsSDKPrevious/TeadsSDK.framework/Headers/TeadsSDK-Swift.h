11074842f16ca5842a37dd4b908e8f7f8806f150
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2019-09-19 06:50:04 -0700

    v4.3.0Previous

--- TeadsSDKPrevious/TeadsSDK.framework/Headers/TeadsSDK-Swift.h
+++ TeadsSDKPrevious/TeadsSDK.framework/Headers/TeadsSDK-Swift.h
@@ -470,6 +470,7 @@ SWIFT_CLASS("_TtC8TeadsSDK15TeadsAdSettings")
 - (void)disableMediaPreload;
 - (void)pageUrl:(NSString * _Nonnull)urlString;
 - (void)userConsentWithSubjectToGDPR:(NSString * _Nonnull)subjectToGDPR consent:(NSString * _Nonnull)consent;
+- (void)disableBatteryMonitoring;
 - (nonnull instancetype)initWithBuild:(SWIFT_NOESCAPE void (^ _Nonnull)(TeadsAdSettings * _Nonnull))build OBJC_DESIGNATED_INITIALIZER;
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_DEPRECATED_MSG("-init is unavailable");
