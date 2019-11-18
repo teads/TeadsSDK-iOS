156eb09a0f098af29a9d085d3411a0f6d7d6ad9c
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2019-11-18 13:24:09 +0000

    v4.4.0

--- TeadsSDK.framework/Headers/OMIDAdSessionConfiguration.h
+++ TeadsSDK.framework/Headers/OMIDAdSessionConfiguration.h
@@ -7,22 +7,35 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * Identifies which integration layer is responsible for sending certain events.
+ */
 typedef NS_ENUM(NSUInteger, OMIDOwner) {
-    OMIDJavaScriptOwner = 1, // will translate into "JAVASCRIPT" when published to the OMID JS service.
-    OMIDNativeOwner = 2, // will translate into "NATIVE" when published to the OMID JS service.
-    OMIDNoneOwner = 3 // will translate into "NONE" when published to the OMID JS service.
+    /** The integration will send the event from a JavaScript session script. */
+    OMIDJavaScriptOwner = 1,
+    /** The integration will send the event from the native layer. */
+    OMIDNativeOwner = 2,
+    /** The integration will not send the event. */
+    OMIDNoneOwner = 3
 };
 
+/**
+ * The ad session configuration supplies the owner for both the impression and video events.
+ * The OMID JS service will use this information to help identify where the source of these
+ * events is expected to be received.
+ */
 @interface OMIDTeadstvAdSessionConfiguration : NSObject
 
 @property OMIDOwner impressionOwner;
 @property OMIDOwner videoEventsOwner;
 @property BOOL isolateVerificationScripts;
 
-/// Returns nil and sets error if OMID isn't active or arguments are invalid.
-/// @param impressionOwner providing details of who is responsible for triggering the impression event.
-/// @param videoEventsOwner providing details of who is responsible for triggering video events. This is only required for video ad sessions and should be set to videoEventsOwner:OMIDNoneOwner for display ad sessions.
-/// @param isolateVerificationScripts determines whether verification scripts will be placed in a sandboxed environment. This will not have any effect for native sessions.
+/**
+ * Returns nil and sets error if OMID isn't active or arguments are invalid.
+ * @param impressionOwner providing details of who is responsible for triggering the impression event.
+ * @param videoEventsOwner providing details of who is responsible for triggering video events. This is only required for video ad sessions and should be set to videoEventsOwner:OMIDNoneOwner for display ad sessions.
+ * @param isolateVerificationScripts determines whether verification scripts will be placed in a sandboxed environment. This will not have any effect for native sessions.
+ */
 - (nullable instancetype)initWithImpressionOwner:(OMIDOwner)impressionOwner
                                 videoEventsOwner:(OMIDOwner)videoEventsOwner
                       isolateVerificationScripts:(BOOL)isolateVerificationScripts
