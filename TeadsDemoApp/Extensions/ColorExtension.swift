ce9517a9e83f4d4fd0daa30c401a84b379573db2
Thibaud Saint-Etienne <thibaud.saint-etienne@teads.tv>
2020-10-06 17:04:55 +0200

    Add colors assets and color extension

---
+++ TeadsDemoApp/Extensions/ColorExtension.swift
@@ -0,0 +1,17 @@
+//
+//  ColorExtension.swift
+//  TeadsDemoApp
+//
+//  Created by Thibaud Saint-Etienne on 06/10/2020.
+//  Copyright © 2020 Teads. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(named: "PrimaryColor")
+    static let lightBlue = UIColor(named: "LightBlueColor")
+    static let teadsBlue = UIColor(named: "TeadsBlueColor")
+    static let teadsPurple = UIColor(named: "TeadsPurpleColor")
+}
