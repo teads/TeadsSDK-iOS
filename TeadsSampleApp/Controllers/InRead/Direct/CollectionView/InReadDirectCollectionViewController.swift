03992d68df84a4304ee26b33122bd0b8da55aace
Paul NICOLAS <89924913+github-paul-nicolas@users.noreply.github.com>
2022-02-15 15:30:03 +0100

    Update TeadsSampleApp/Controllers/InRead/Direct/CollectionView/InReadDirectCollectionViewController.swift

--- TeadsSampleApp/Controllers/InRead/Direct/CollectionView/InReadDirectCollectionViewController.swift
+++ TeadsSampleApp/Controllers/InRead/Direct/CollectionView/InReadDirectCollectionViewController.swift
@@ -22,7 +22,7 @@ class InReadDirectCollectionViewController: TeadsViewController {
     let contentCell = "TeadsContentCell"
     let teadsAdCellIndentifier = "TeadsAdCell"
     let fakeArticleCell = "fakeArticleCell"
-    let trackerViewItemNumber = 2
+    let trackerViewItemNumber = 2 // tracker view needs to be placed above the slot view
     var adItemNumber: Int {
         return trackerViewItemNumber + 1
     }
