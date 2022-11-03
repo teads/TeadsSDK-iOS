0b8b3be9abc392b1b312fe9af58c21ad80f7ed49
Thibaud Saint-Etienne <89925093+github-thibaud-saintetienne@users.noreply.github.com>
2022-11-03 15:15:54 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@
 import PackageDescription
 
 let teadsModuleName = "TeadsSDK"
+let teadsAdmobAdapterModuleName = "TeadsAdmobAdapter"
 let omModuleName = "OMSDK_Teadstv"
 
 let package = Package(
@@ -15,15 +16,27 @@ let package = Package(
             name: teadsModuleName,
             targets: [teadsModuleName, omModuleName]
         ),
+        .library(
+            name: teadsAdmobAdapterModuleName,
+            targets: [teadsAdmobAdapterModuleName]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "9.0.0"),
+        .package(url: "https://github.com/teads/TeadsSDK-iOS.git", from: "5.0.20"),
     ],
     targets: [
+        .target(
+            name: teadsAdmobAdapterModuleName,
+            dependencies: ["GoogleMobileAds", "TeadsSDK"]
+        ),
         .binaryTarget(
             name: teadsModuleName,
-            path: "Frameworks/\(teadsModuleName).xcframework"
+            path: "\(teadsModuleName).xcframework"
         ),
         .binaryTarget(
             name: omModuleName,
-            path: "Frameworks/\(omModuleName).xcframework"
+            path: "\(omModuleName).xcframework"
         ),
     ]
 )
