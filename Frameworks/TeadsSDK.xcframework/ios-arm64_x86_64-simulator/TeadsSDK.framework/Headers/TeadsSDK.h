f45bbd6d49acaf52d35fe1b2e506c4e484c9183f
Anka <runner@iad20-gt1024-687b2512-9420-49bc-a019-e35d07e17a62-FAAE8344D0B3.local>
2025-10-29 14:07:08 +0000

    Release new version: 6.0.0

--- Frameworks/TeadsSDK.xcframework/ios-arm64_x86_64-simulator/TeadsSDK.framework/Headers/TeadsSDK.h
+++ Frameworks/TeadsSDK.xcframework/ios-arm64_x86_64-simulator/TeadsSDK.framework/Headers/TeadsSDK.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double TeadsSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char TeadsSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TeadsSDK/PublicHeader.h>
-
-
