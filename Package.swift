b04144905395916e7b578b8b89860d3b7c2f45b3
Leo Lemesev <leonid.lemesev@teads.com>
2026-04-06 19:37:28 +0300

    Restore mediation adapters to SPM Package.swift
    Adapters were accidentally removed in the 6.0.11 release. This restores
    TeadsAdMobAdapter, TeadsAppLovinAdapter, TeadsPBMPluginRenderer, and
    TeadsAdapterCommon targets with their external dependencies.

--- Package.swift
+++ Package.swift
@@ -3,10 +3,19 @@
 import PackageDescription
 
 let teadsModuleName = "TeadsSDK"
+let teadsAdMobAdapterModuleName = "TeadsAdMobAdapter"
+let teadsAppLovinAdapterModuleName = "TeadsAppLovinAdapter"
+let teadsPBMPluginRendererModuleName = "TeadsPBMPluginRenderer"
+let mediationAdaptersDirectory = "MediationAdapters"
+let googleMobileAdsModuleName = "GoogleMobileAds"
+let appLovinMaxModuleName = "AppLovinSDK"
+let prebidMobileModuleName = "PrebidMobile"
 let omModuleName = "OMSDK_Teadstv"
+let commonModuleName = "TeadsAdapterCommon"
+let commonModuleNamePath = "Common"
 
 let package = Package(
-    name: teadsModuleName,
+    name: "Teads",
     platforms: [
         .iOS(.v14),
     ],
@@ -14,6 +23,35 @@ let package = Package(
         .library(
             name: teadsModuleName,
             targets: [teadsModuleName, omModuleName]
+        ),
+        .library(
+            name: teadsAdMobAdapterModuleName,
+            targets: [teadsAdMobAdapterModuleName]
+        ),
+        .library(
+            name: teadsAppLovinAdapterModuleName,
+            targets: [teadsAppLovinAdapterModuleName]
+        ),
+        .library(
+            name: teadsPBMPluginRendererModuleName,
+            targets: [teadsPBMPluginRendererModuleName]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: googleMobileAdsModuleName,
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            "12.2.0" ..< "13.0.0"
+        ),
+        .package(
+            name: appLovinMaxModuleName,
+            url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git",
+            "13.3.1" ..< "14.0.0"
+        ),
+        .package(
+            name: prebidMobileModuleName,
+            url: "https://github.com/prebid/prebid-mobile-ios.git",
+            "3.0.2" ..< "4.0.0"
         ),
     ],
     targets: [
@@ -24,6 +62,39 @@ let package = Package(
         .binaryTarget(
             name: omModuleName,
             path: "Frameworks/\(omModuleName).xcframework"
+        ),
+        .target(
+            name: commonModuleName,
+            dependencies: [
+                .target(name: teadsModuleName),
+                .target(name: omModuleName),
+            ],
+            path: "\(mediationAdaptersDirectory)/\(commonModuleNamePath)"
+        ),
+        .target(
+            name: teadsAdMobAdapterModuleName,
+            dependencies: [
+                .product(name: googleMobileAdsModuleName, package: googleMobileAdsModuleName),
+                .target(name: commonModuleName),
+            ],
+            path: "\(mediationAdaptersDirectory)/\(teadsAdMobAdapterModuleName)"
+        ),
+        .target(
+            name: teadsAppLovinAdapterModuleName,
+            dependencies: [
+                .product(name: appLovinMaxModuleName, package: appLovinMaxModuleName),
+                .target(name: commonModuleName),
+            ],
+            path: "\(mediationAdaptersDirectory)/\(teadsAppLovinAdapterModuleName)"
+        ),
+        .target(
+            name: teadsPBMPluginRendererModuleName,
+            dependencies: [
+                .product(name: prebidMobileModuleName, package: prebidMobileModuleName),
+                .target(name: teadsModuleName),
+                .target(name: omModuleName),
+            ],
+            path: "\(mediationAdaptersDirectory)/\(teadsPBMPluginRendererModuleName)"
         ),
     ]
 )
